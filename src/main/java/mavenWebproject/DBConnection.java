d6263fc74c5a8b9ed724b1b670a416c7ce03f790
murulii <muruli800@gmail.com>
2023-11-24 12:52:04 +0530

    Update DBConnection.java

--- src/main/java/mavenWebproject/DBConnection.java
+++ src/main/java/mavenWebproject/DBConnection.java
@@ -9,11 +9,11 @@ public class DBConnection {
         String jdbcUrl = System.getenv("host"); // Use your own environment variable name
         String username = System.getenv("user"); // Use your own environment variable name
         String password = System.getenv("pass"); // Use your own environment variable name
-       
+        String database = System.getenv("database");
 
-        String fullJdbcUrl = jdbcUrl + "/" + mydb;
-        
+        // The JDBC URL should include the database name
+        String fullJdbcUrl = jdbcUrl + "/" + database;
 
-        return DriverManager.getConnection(fullJdbcUrl, username, password);
+        return DriverManager.getConnection(fullJdbcUrl, username, password)
     }
 }
